aedc39657f5002a2a29e98ceb5791e7a40bf6394
luisAlcides <luisalcidesblandon@gmail.com>
2024-11-25 20:16:10 -0600

    allow hossts

--- etica_proyecto/settings.py
+++ etica_proyecto/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG")
 
 
-ALLOWED_HOSTS = ['railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['railway.app', '127.0.0.1', 'https://etica.up.railway.app/']
 
 
 # Application definition
