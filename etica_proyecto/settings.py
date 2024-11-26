fe5f10cff77fc15c996cef93c13b8de07d712103
luisAlcides <luisalcidesblandon@gmail.com>
2024-11-25 21:00:52 -0600

    Trust

--- etica_proyecto/settings.py
+++ etica_proyecto/settings.py
@@ -135,4 +135,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ['http://*','https://etica.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['http://*','https://etica.up.railway.app/', 'etica.up.railway.app']
